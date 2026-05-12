c6dd99d0b6f7d940e1b5d9d7c4698feded123d68
palratiraj <palratiraj@gmail.com>
2026-05-13 01:34:05 +0530

    Added 25 ancient history MCQs
    Added multiple-choice questions related to ancient history, including questions about the Indus Valley Civilization, Maurya Empire, and notable historical figures.

---
+++ app/quiz/history/ancientHistoryMcq.js
@@ -0,0 +1,253 @@
+export const ancientHistoryMcq = [
+  {
+  question: "The Great Bath was discovered at which Indus Valley site?",
+  options: [
+    "Harappa",
+    "Mohenjo-Daro",
+    "Lothal",
+    "Kalibangan",
+  ],
+  answer: "Mohenjo-Daro",
+},
+{
+  question: "Who founded the Maurya Empire?",
+  options: [
+    "Ashoka",
+    "Bindusara",
+    "Chandragupta Maurya",
+    "Harsha",
+  ],
+  answer: "Chandragupta Maurya",
+},
+{
+  question: "Which Veda is the oldest Veda?",
+  options: [
+    "Yajurveda",
+    "Rigveda",
+    "Samaveda",
+    "Atharvaveda",
+  ],
+  answer: "Rigveda",
+},
+{
+  question: "The capital of Magadha was:",
+  options: [
+    "Pataliputra",
+    "Taxila",
+    "Ujjain",
+    "Kannauj",
+  ],
+  answer: "Pataliputra",
+},
+{
+  question: "Ashoka spread which religion after the Kalinga War?",
+  options: [
+    "Jainism",
+    "Buddhism",
+    "Hinduism",
+    "Zoroastrianism",
+  ],
+  answer: "Buddhism",
+},
+{
+  question: "Who was the founder of Buddhism?",
+  options: [
+    "Mahavira",
+    "Gautama Buddha",
+    "Ashoka",
+    "Chanakya",
+  ],
+  answer: "Gautama Buddha",
+},
+{
+  question: "The famous university of ancient India was:",
+  options: [
+    "Nalanda",
+    "Delhi University",
+    "Takshashila College",
+    "Banaras Hindu University",
+  ],
+  answer: "Nalanda",
+},
+{
+  question: "Who wrote the Arthashastra?",
+  options: [
+    "Kalidasa",
+    "Chanakya",
+    "Panini",
+    "Aryabhata",
+  ],
+  answer: "Chanakya",
+},
+{
+  question: "The Harappan civilization belonged to which age?",
+  options: [
+    "Iron Age",
+    "Bronze Age",
+    "Stone Age",
+    "Copper Age",
+  ],
+  answer: "Bronze Age",
+},
+{
+  question: "Who was the last ruler of the Maurya dynasty?",
+  options: [
+    "Ashoka",
+    "Brihadratha",
+    "Bindusara",
+    "Samudragupta",
+  ],
+  answer: "Brihadratha",
+},
+{
+  question: "The Kalinga War was fought by:",
+  options: [
+    "Harsha",
+    "Chandragupta Maurya",
+    "Ashoka",
+    "Samudragupta",
+  ],
+  answer: "Ashoka",
+},
+{
+  question: "Which Gupta ruler was called the Napoleon of India?",
+  options: [
+    "Chandragupta I",
+    "Samudragupta",
+    "Skandagupta",
+    "Kumaragupta",
+  ],
+  answer: "Samudragupta",
+},
+{
+  question: "Who founded Jainism?",
+  options: [
+    "Mahavira",
+    "Buddha",
+    "Ashoka",
+    "Bindusara",
+  ],
+  answer: "Mahavira",
+},
+{
+  question: "The Ajanta caves are famous for:",
+  options: [
+    "Paintings",
+    "Weapons",
+    "Coins",
+    "Temples only",
+  ],
+  answer: "Paintings",
+},
+{
+  question: "Which river was associated with the Indus Valley Civilization?",
+  options: [
+    "Ganga",
+    "Yamuna",
+    "Indus",
+    "Godavari",
+  ],
+  answer: "Indus",
+},
+{
+  question: "Who was the court poet of Chandragupta II?",
+  options: [
+    "Kalidasa",
+    "Banabhatta",
+    "Tulsidas",
+    "Panini",
+  ],
+  answer: "Kalidasa",
+},
+{
+  question: "The Iron Pillar is located in:",
+  options: [
+    "Agra",
+    "Delhi",
+    "Patna",
+    "Jaipur",
+  ],
+  answer: "Delhi",
+},
+{
+  question: "Which dynasty built the Khajuraho temples?",
+  options: [
+    "Maurya",
+    "Chola",
+    "Chandela",
+    "Gupta",
+  ],
+  answer: "Chandela",
+},
+{
+  question: "The main occupation of the Harappans was:",
+  options: [
+    "Hunting",
+    "Fishing",
+    "Agriculture",
+    "Mining",
+  ],
+  answer: "Agriculture",
+},
+{
+  question: "Who was the founder of the Gupta Empire?",
+  options: [
+    "Samudragupta",
+    "Chandragupta I",
+    "Skandagupta",
+    "Kanishka",
+  ],
+  answer: "Chandragupta I",
+},
+{
+  question: "Kanishka was a ruler of which dynasty?",
+  options: [
+    "Maurya",
+    "Kushan",
+    "Gupta",
+    "Chola",
+  ],
+  answer: "Kushan",
+},
+{
+  question: "The earliest inscriptions were written in:",
+  options: [
+    "Brahmi script",
+    "Devanagari",
+    "Persian",
+    "Tamil",
+  ],
+  answer: "Brahmi script",
+},
+{
+  question: "Which ancient Indian scholar wrote Ashtadhyayi?",
+  options: [
+    "Kalidasa",
+    "Panini",
+    "Aryabhata",
+    "Charaka",
+  ],
+  answer: "Panini",
+},
+{
+  question: "The Battle of Hydaspes was fought between Alexander and:",
+  options: [
+    "Ashoka",
+    "Porus",
+    "Harsha",
+    "Kanishka",
+  ],
+  answer: "Porus",
+},
+{
+  question: "Which Chola ruler built the Brihadeshwara Temple?",
+  options: [
+    "Rajaraja Chola I",
+    "Ashoka",
+    "Pulakeshin II",
+    "Harsha",
+  ],
+  answer: "Rajaraja Chola I",
+},
+  ];
+
