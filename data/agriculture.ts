5edb756c853eab06f12f3b9bafb6686b63d69799
palratiraj <palratiraj@gmail.com>
2026-05-15 22:05:35 +0530

    Update agriculture.ts

--- data/agriculture.ts
+++ data/agriculture.ts
@@ -1,5 +1,29 @@
 export const agricultureQuestions = [
   {
+    export const agricultureQuestions = [
+  {
+    question: "Which crop is known as golden fiber?",
+    options: [
+      "Cotton",
+      "Jute",
+      "Tea",
+      "Rice"
+    ],
+    answer: "Jute",
+  },
+
+  {
+    question: "Which fertilizer contains nitrogen?",
+    options: [
+      "Urea",
+      "Gypsum",
+      "Lime",
+      "Potash"
+    ],
+    answer: "Urea",
+  },
+
+  // OLD QUESTIONS BELOW
     question: "Green Revolution related to?",
     options: ["Milk","Agriculture","Industry","Transport"],
     answer: "Agriculture",
