d478611ebff4c94e8d278ed42e93ecfa32b2f3b5
lakshyacivilservices <palratiraj@gmail.com>
2026-05-17 14:01:21 +0530

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,17 @@
+export default function Home() {
+  return (
+    <main
+      style={{
+        padding: "40px",
+        fontFamily: "Arial",
+      }}
+    >
+      <h1>Lakshya Civil Services Academy 🚀</h1>
+
+      <p>
+        Free MCQs for UGC NET, Psychology,
+        UPPSC, PET, SSC GK and more.
+      </p>
+    </main>
+  );
+}
