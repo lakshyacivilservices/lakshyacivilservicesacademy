278053238ca0cfafc1d1b6d3fa9ae9794d50026f
lakshyacivilservices <palratiraj@gmail.com>
2026-05-17 16:45:30 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,28 @@
 export default function Home() {
   return (
-    <main
-      style={{
-        padding: "40px",
-        fontFamily: "Arial",
-      }}
-    >
+    <main style={{ padding: "40px", fontFamily: "Arial" }}>
       <h1>Lakshya Civil Services Academy 🚀</h1>
 
       <p>
-        Free MCQs for UGC NET, Psychology,
-        UPPSC, PET, SSC GK and more.
+        Free MCQs for UPPSC, UGC NET,
+        Psychology, SSC GK and more.
       </p>
+
+      <a
+        href="/quiz/uppsc"
+        style={{
+          display: "inline-block",
+          marginTop: "20px",
+          padding: "15px 25px",
+          background: "#2563eb",
+          color: "white",
+          borderRadius: "10px",
+          textDecoration: "none",
+          fontWeight: "bold",
+        }}
+      >
+        Start UPPSC Quiz
+      </a>
     </main>
   );
 }
